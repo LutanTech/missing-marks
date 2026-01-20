62fb44139e6fc8a2c376a3b6cc8257206775cf8a
Lutan <lutancorpinfoteam@gmail.com>
2026-01-20 16:55:29 +0300

    https update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 // const API_URL = "http://localhost:7820/submission/missing-marks";
-const API_URL = "http://missingmarks.eu.pythonanywhere.com/submission/missing-marks";
+const API_URL = "https://missingmarks.eu.pythonanywhere.com/submission/missing-marks";
 let autoCloseTimer;
 
 function showModal(type, title, message) {
